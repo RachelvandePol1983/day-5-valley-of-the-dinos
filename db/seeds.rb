d7fe22c58a9e9761295bfe844c6a85968d0f9b8a
Rachel van de Pol <rachelvdpol@gmail.com>
2016-10-06 11:45:59 +0200

    Filling database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,13 @@
+dino1 = Dinosaur.create( { name: "Iguanodon", age: 3, image_url: "http://images.dinosaurpictures.org.rsz.io/iguanodon_b35b.jpg" } )
+dino2 = Dinosaur.create( { name: "Mosasaurus", age: 10, image_url: "http://images.dinosaurpictures.org.rsz.io/mosasaurus_hoffmani_by_olorotitan-d4esmfm_5c48.jpg" } )
+dino3 = Dinosaur.create( { name: "Hungarosaurus", age: 15, image_url: "http://images.dinosaurpictures.org.rsz.io/nyito_4f23.jpg" } )
+dino4 = Dinosaur.create( { name: "Yvontrauporus", age: 40, image_url: "http://www.kids-dinosaurs.com/images/dinosaurs-triceratops.jpg"})
+
 # This file should contain all the record creation needed to seed the database with its default values.
+
+
+
+
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
